25725b071293e9bed568da207e2b4e26949669b4
Krzysztof Biskup <k.biskup.ns@gmail.com>
2019-11-13 14:51:44 +0100

    done

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
-var movies = [
+var filmsList = [
     {
         id: 1,
-        title: 'Harry Potter',
-        descdescription: 'film o czarodzieju'
+        title: 'Testowy film 1',
+        description: 'Testowy opis 1',
+        img: 'https://cdn.pixabay.com/photo/2019/09/11/22/29/leaf-4470075__480.jpg'
     },
     {
         id: 2,
-        title: 'Król Lew',
-        description: 'Film o królu sawanny'
+        title: 'Testowy film 2',
+        description: 'Testowy film 2',
+        img: 'https://cdn.pixabay.com/photo/2019/09/11/22/29/leaf-4470075__480.jpg'
     },
     {
         id: 3,
-        title: 'Kurier',
-        description: 'Nie wiem o czym jest ten film',
-        img: 'https://ssl-gfx.filmweb.pl/po/01/92/810192/7874217.3.jpg',
+        title: 'Testowy film 3',
+        description: 'Testowy film 3',
+        img: 'https://cdn.pixabay.com/photo/2019/09/11/22/29/leaf-4470075__480.jpg'
     }
 ];
-var moviesElements = movies.map(function(movie){
-    return React.createElement('li', {key: movie.id},
-        React.createElement('h2', {}, movie.title),
-        React.createElement('p', {}, movie.description),
-        React.createElement('img', {src: movie.img})
-    );
+
+//console.log(moviesList);
+
+var Movie = React.createClass ({
+    render: function() {
+        return React.createElement ('li', {}, 
+            React.createElement('h2', {}, this.props.title),
+            React.createElement('p', {}, this.props.description),
+            React.createElement('img', {src: this.props.img})
+        )
+    }
 });
 
-var element = 
-    React.createElement('div', {},
-        React.createElement('h1', {}, 'Lista filmów'),
-        React.createElement('ul', {}, moviesElements)
-    );
+var MoviesList = React.createClass ({
+    render: function() {
+        var MoviesListElements = filmsList.map(function(film){ 
+            return React.createElement(Movie, {key:film.id, title:film.title, description:film.description, img:film.img})    
+        })
+    
+        return React.createElement('ul', {}, MoviesListElements)
+    }
+});
 
+//console.log(MoviesList);
+var element = React.createElement(MoviesList, {item: filmsList})
+//console.log(element);
 ReactDOM.render(element, document.getElementById('app'));
