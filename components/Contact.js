2ecb5872709ce0439bc166c0b1fcaf578d0a975b
Krzysztof Biskup <k.biskup.ns@gmail.com>
2019-11-20 23:13:58 +0100

    ready for review

---
+++ components/Contact.js
@@ -0,0 +1,23 @@
+var Contact = React.createClass({
+    propTypes: {
+      item: React.PropTypes.object.isRequired,
+    },
+  
+    render: function() {
+      return (
+        React.createElement('div', {className: 'contactItem'},
+          React.createElement('img', {
+            className: 'contactImage',
+            src: 'https://img.icons8.com/carbon-copy/100/000000/contact-card.png'
+          }),
+          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+          React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+            this.props.item.email
+          )
+        )
+      )
+    },
+  });
+
+
