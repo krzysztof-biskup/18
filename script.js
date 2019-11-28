95438c3a2e35f2b5fb59bf5980af6b584628f150
Krzysztof Biskup <k.biskup.ns@gmail.com>
2019-11-28 22:34:37 +0100

    Revert "18.5"

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-var app = React.createElement(App);
-ReactDOM.render(app, document.getElementById('app'));
-
-
-
-=======
 var filmsList = [
     {
         id: 1,
@@ -51,4 +45,3 @@ var MoviesList = React.createClass ({
 var element = React.createElement(MoviesList, {item: filmsList})
 //console.log(element);
 ReactDOM.render(element, document.getElementById('app'));
-
