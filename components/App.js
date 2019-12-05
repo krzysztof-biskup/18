df7c7052771a3704667041b927d03076cc8e4d1d
Krzysztof Biskup <k.biskup.ns@gmail.com>
2019-12-05 17:30:06 +0100

    Update App.js

--- components/App.js
+++ components/App.js
@@ -7,10 +7,10 @@ var contactForm = {
 var App = React.createClass({
   render: function() {
     return (
-      <div className={'form-control'}>{
-        React.createElement(ContactForm, {contact: contactForm}),
-        React.createElement(Contacts, {items: contacts}, {}) }
-      </div>
+      <div className={'app form-control'}> (      
+        {React.createElement(ContactForm, {contact: contactForm}),
+        React.createElement(Contacts, {items: contacts}, {})}
+      )</div>
     )
   }
 }); 
