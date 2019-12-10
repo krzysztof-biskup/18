ba28297bdd6a34b7c58639fd6e00568739eaea7b
Krzysztof Biskup <k.biskup.ns@gmail.com>
2019-12-10 16:39:26 +0100

    ready for rewiev

--- components/Contact.js
+++ components/Contact.js
@@ -5,17 +5,6 @@ var Contact = React.createClass({
   
   render: function() {
     return (
-     /* React.createElement('div', {className: 'contactItem'},
-        React.createElement('img', {
-          className: 'contactImage',
-          src: 'https://img.icons8.com/carbon-copy/100/000000/contact-card.png'
-        }),
-        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-        React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-          this.props.item.email
-        )
-      */
       <div className={'contactItem'} >
         <img className={'contactImage'} src={'https://img.icons8.com/carbon-copy/100/000000/contact-card.png'}/>
         <p className={'contactLabel'}>
