d425bf9e5b7e2be39d2c1d41d2c676ce2f883a35
Krzysztof Biskup <k.biskup.ns@gmail.com>
2019-12-10 18:57:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,38 +2,42 @@ var movies = [
     {
         id: 1,
         title: 'Harry Potter',
-        desc: 'film o czarodzieju'
+        desc: 'film o czarodzieju',
+        img: 'https://ssl-gfx.filmweb.pl/po/01/92/810192/7874217.3.jpg'
     },
     {
         id: 2,
         title: 'Król Lew',
-        desc: 'Film o królu sawanny'
+        desc: 'Film o królu sawanny',
+        img: 'https://ssl-gfx.filmweb.pl/po/01/92/810192/7874217.3.jpg'
     },
     {
         id: 3,
         title: 'Kurier',
         desc: 'Nie wiem o czym jest ten film',
-        img: 'https://ssl-gfx.filmweb.pl/po/01/92/810192/7874217.3.jpg',
+        img: 'https://ssl-gfx.filmweb.pl/po/01/92/810192/7874217.3.jpg'
     }
 ];
 
-var moviesElements = movies.map(function(movie){
-    return React.createElement(Movie, {movie: movie})    
-})
-
 var Movie = React.createClass({
     render: function() {
+        return (
         React.createElement('li', {key: this.props.movie.id},
             React.createElement('h2', {}, this.props.movie.title),
             React.createElement('p', {}, this.props.movie.desc),
             React.createElement('img', {src: this.props.movie.img})
         )
+        )
     }
-})
+});
 
 
-var Element = React.createClass({
+
+var Movies = React.createClass({
     render: function(){
+        var moviesElements = movies.map(function(movie){
+            return React.createElement(Movie, {movie: movie},)  
+        });
         return ( 
             React.createElement('div', {},
                 React.createElement('h1', {}, 'Lista filmów'),
@@ -41,7 +45,7 @@ var Element = React.createClass({
             )
         )
     }
-}),
+});
 
-var app = React.createElement(Element);
+var app = React.createElement(Movies);
 ReactDOM.render(app, document.getElementById('app'));
